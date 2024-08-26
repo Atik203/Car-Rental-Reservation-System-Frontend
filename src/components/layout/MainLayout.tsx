6f28ddd521204b50874adfd0c7406d723c7824ae
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-26 22:10:02 +0600

    added dark light theme

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,15 +1,11 @@
 import { Outlet } from "react-router-dom";
-import { ModeToggle } from "../ui/mode-toggle";
-import { ThemeProvider } from "../ui/theme-provider";
 
 const MainLayout = () => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div>
-        <ModeToggle />
-        <Outlet />
-      </div>
-    </ThemeProvider>
+    <div>
+      {/* <ModeToggle /> */}
+      <Outlet />
+    </div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -12,38 +12,41 @@ import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/ui/theme-provider";
 import "./index.css";
 import { persistor, store } from "./redux/store.ts";
 import router from "./routes/routes.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PersistGate loading={null} persistor={persistor}>
-      <Provider store={store}>
-        <Toaster
-          visibleToasts={10}
-          richColors={true}
-          toastOptions={{
-            classNames: {
-              success: "toast-success toast-common",
-              error: "toast-error toast-common",
-              warning: "toast-warning toast-common",
-              info: "toast-info toast-common",
-            },
-          }}
-          icons={{
-            success: <CheckBadgeIcon className="w-6 h-6" />,
-            info: <InformationCircleIcon className="w-6 h-6" />,
-            warning: <ExclamationCircleIcon className="w-6 h-6" />,
-            error: <ExclamationTriangleIcon className="w-6 h-6" />,
-            loading: <MagnifyingGlassCircleIcon className="w-6 h-6" />,
-          }}
-        />
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <PersistGate loading={null} persistor={persistor}>
+        <Provider store={store}>
+          <Toaster
+            visibleToasts={10}
+            richColors={true}
+            toastOptions={{
+              classNames: {
+                success: "toast-success toast-common",
+                error: "toast-error toast-common",
+                warning: "toast-warning toast-common",
+                info: "toast-info toast-common",
+              },
+            }}
+            icons={{
+              success: <CheckBadgeIcon className="w-6 h-6" />,
+              info: <InformationCircleIcon className="w-6 h-6" />,
+              warning: <ExclamationCircleIcon className="w-6 h-6" />,
+              error: <ExclamationTriangleIcon className="w-6 h-6" />,
+              loading: <MagnifyingGlassCircleIcon className="w-6 h-6" />,
+            }}
+          />
 
-        <HelmetProvider>
-          <RouterProvider router={router} />
-        </HelmetProvider>
-      </Provider>
-    </PersistGate>
+          <HelmetProvider>
+            <RouterProvider router={router} />
+          </HelmetProvider>
+        </Provider>
+      </PersistGate>
+    </ThemeProvider>
   </React.StrictMode>
 );
