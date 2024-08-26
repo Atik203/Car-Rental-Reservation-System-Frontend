2d2b5aaad83696a25d19f56686ec8cfd273ba206
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-26 20:02:00 +0600

    setup the redux

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -7,8 +7,10 @@ import {
   FetchArgs,
   fetchBaseQuery,
 } from "@reduxjs/toolkit/query/react";
+import { toast } from "sonner";
 import { logout, setUser } from "../features/auth/authSlice";
 import { RootState } from "../store";
+
 const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:5000/api/v1",
   credentials: "include",
@@ -28,6 +30,12 @@ const baseQueryWithRefreshToken: BaseQueryFn<
 > = async (args, api, extraOptions): Promise<any> => {
   let result = await baseQuery(args, api, extraOptions);
 
+  if (result.error?.status === 404) {
+    toast.error((result?.error?.data as { message?: string })?.message);
+  }
+  if (result.error?.status === 403) {
+    toast.error((result?.error?.data as { message?: string })?.message);
+  }
   if (result.error?.status === 401) {
     const res = await fetch("http://localhost:5000/api/v1/auth/refresh-token", {
       method: "POST",
