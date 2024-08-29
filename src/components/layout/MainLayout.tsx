70159ab4857dce7b43cf3e7b3dc85eeca54877c9
Atikur Rahaman <atikurrahaman0305@gmail.com>
2024-08-29 23:18:11 +0600

    added banner

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Banner from "../ui/custom/customUI/Banner";
 import Footer from "../ui/custom/others/Footer";
 import Navbar from "../ui/custom/others/Navbar";
 
 const MainLayout = () => {
   return (
     <div className="mx-auto">
+      <Banner />
       <Navbar />
       <div className="max-w-6xl mx-auto min-h-screen">
         <Outlet />
